4c02afcb7893ef82eb224c1eeb8452bd935ed33c
Soumya Pattanayak <soumyapattanayak@Soumyas-iMac.local>
2024-11-25 09:40:15 +1100

    update exception

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,6 @@
 import sys  # Module provides functions and variables that are used to manipulate different parts of Python runtime environment
-import logging
-
+#import logging
+from src.logger import logging
 def error_message_details(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
@@ -15,12 +15,12 @@ class CustomException(Exception):
     
     def __str__(self):
         return self.error_message
-    '''
+'''   
 if __name__=="__main__":
     try:
         a = 1/0
     except Exception as e:
-        logging.info("Divide By Zero Error")
+        logging.info("Divide by Zero")
         raise CustomException(e,sys)
+'''
         
-        '''
